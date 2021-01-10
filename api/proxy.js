1bae5444a18ef2165b042a2a7aca8cf398e64827
ChonLam <1685020647@qq.com>
2021-01-10 16:36:33 +0800

    add proxy

---
+++ api/proxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  if (req.url.startsWith('/boss' || '/front')) {
+    target = 'http://eduboss.lagou.com'
+  }
+
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+  })(req, res)
+}
